4237d8ee6bd0bd0d9178ca5ba4861d1b1bb2d101
Philipp Harnisch <p.harnisch.privat@gmail.com>
2021-12-01 17:24:01 +0100

    WIP

---
+++ training/model_configs.py
@@ -0,0 +1,75 @@
+from enum import Enum
+import transformers
+
+class TransformerType(Enum):
+    BERT = 1
+    ROBERTA = 2
+    ELECTRA = 3
+
+class Transformer:
+    tokenizer = None
+    config: transformers.PretrainedConfig = None
+    model: transformers.PreTrainedModel = None
+
+    def __init__(
+            self,
+            tokenizer: transformer.Tokenizer,
+            conf: transformers.PretrainedConfig,
+            model: transformers.PreTrainedModel,
+    ):
+        self.config = conf
+
+    @staticmethod
+    def get_transformer(transformer_type: TransformerType):
+        if transformer_type == TransformerType.BERT:
+            conf = transformers.BertConfig(
+                vocab_size=30_522,  # we align this to the tokenizer vocab_size
+                max_position_embeddings=512,
+                hidden_size=768,
+                num_attention_heads=12,
+                num_hidden_layers=6,
+                type_vocab_size=1
+            )
+            return Transformer(
+                tokenizer=transformers.BertTokenizer.from_pretrained('../models/word_piece_tokenizer', max_len=512),
+                conf=conf,
+                model=transformers.BertForMaskedLM(conf)
+            )
+        elif transformer_type == TransformerType.ELECTRA:
+            conf = transformers.ElectraConfig(
+                vocab_size=30_522,  # we align this to the tokenizer vocab_size
+                max_position_embeddings=512,  # 513?
+                hidden_size=256,
+                num_attention_heads=4,
+                num_hidden_layers=6,  # this change is only because of computational reduction in prototype
+                type_vocab_size=1  # this change makes sense (?)
+            )
+            return Transformer(
+                tokenizer=transformers.ElectraTokenizer.from_pretrained('../models/word_piece_tokenizer', max_len=512),
+                conf=conf,
+                model=transformers.ElectraForMaskedLM(conf)
+            )
+        elif transformer_type == TransformerType.ROBERTA:
+            conf = transformers.RobertaConfig(
+                vocab_size=30_522,  # we align this to the tokenizer vocab_size
+                max_position_embeddings=512,
+                hidden_size=768,
+                num_attention_heads=12,
+                num_hidden_layers=6,
+                type_vocab_size=1
+            )
+            return Transformer(
+                tokenizer=transformers.RobertaTokenizer.from_pretrained('../models/byte_level_bpe_tokenizer', max_len=512),
+                conf=conf,
+                model=transformers.RobertaForMaskedLM(conf)
+            )
+        else:
+            raise Exception(f"TransformerType {type} not implemented!")
+
+#class BertTransformer(Transformer):
+
+
+
+
+
+
