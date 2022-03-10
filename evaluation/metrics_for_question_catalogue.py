f9caf7235583466f302c51621f1885ebe4faefea
Philipp Harnisch <p.harnisch.privat@gmail.com>
2022-03-10 11:01:21 +0100

    debug

--- evaluation/metrics_for_question_catalogue.py
+++ evaluation/metrics_for_question_catalogue.py
@@ -154,9 +154,9 @@ class MetricCalculator(abc.ABC):
         for idx, bucket in enumerate(buckets):
             borders = (bucket[0]["relative_frequency"], bucket[-1]["relative_frequency"])
             bucket_borders.append(borders)
+        print(buckets)
         buckets_2 = [[m["reciprocal_rank"] for m in bucket] for bucket in buckets]
         buckets = [[m["prediction_confidence"] for m in bucket] for bucket in buckets]
-        print(buckets_2)
         for idx, borders in enumerate(bucket_borders):
             avg = sum(buckets[idx])/len(buckets[idx]) if len(buckets[idx]) > 0 else -1
             avg_2 = sum(buckets_2[idx])/len(buckets_2[idx]) if len(buckets_2[idx]) > 0 else -1
