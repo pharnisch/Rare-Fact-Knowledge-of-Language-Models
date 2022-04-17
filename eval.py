35dc7a0f87b722c6b57b4cb9c3822d7c67f9b243
Philipp Harnisch <p.harnisch.privat@gmail.com>
2022-04-17 19:43:10 +0200

    test more pretrained models

--- eval.py
+++ eval.py
@@ -50,8 +50,8 @@ def evaluate():
         tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased")
         model = AutoModelForMaskedLM.from_pretrained("bert-large-uncased")
     elif args.checkpoint == "distil_pretrained":
-        from transformers import AutoTokenizer, AutoModelForMaskedLM, DistilBertForMaskedLM
-        tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
+        from transformers import AutoTokenizer, AutoModelForMaskedLM, DistilBertForMaskedLM, DistilBertTokenizer
+        tokenizer = DistilBertTokenizer.from_pretrained("distilgpt2")
         model = DistilBertForMaskedLM.from_pretrained("distilgpt2")
     elif "_pretrained" in args.checkpoint:
         from transformers import AutoTokenizer, AutoModelForMaskedLM
