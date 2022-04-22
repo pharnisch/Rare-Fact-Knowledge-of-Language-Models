93376323992b9e9eda6f7b0272b5fba20a78074f
Philipp Harnisch <p.harnisch.privat@gmail.com>
2022-04-23 00:36:17 +0200

    remove randomness in use_trainer

--- use_trainer.py
+++ use_trainer.py
@@ -1,18 +1,19 @@
-from transformers import BertModel, BertConfig, BertTokenizer, BertForPreTraining
+from transformers import BertConfig, BertTokenizer, BertForPreTraining
 from transformers import DataCollatorForLanguageModeling
-from transformers.data.datasets.language_modeling import TextDatasetForNextSentencePrediction
 from transformers import Trainer, TrainingArguments
 import transformers
-
 transformers.set_seed(1337)
+
+#from transformers.data.datasets.language_modeling import TextDatasetForNextSentencePrediction
+from training.data.language_modeling import TextDatasetForNextSentencePrediction
 
 model = BertForPreTraining(BertConfig())
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
 dataset = TextDatasetForNextSentencePrediction(
     tokenizer=tokenizer,
-    file_path="./training/data/nsp/wikipedia/20200501.en/text_for_nsp.txt_0",
-    block_size=512,  # TODO: what does this, what number is suitable for me?
+    file_path="./training/data/wikipedia/20200501.en/nsp/text_for_nsp.txt",
+    block_size=512,
     overwrite_cache=False,
     short_seq_probability=0.1,
     nsp_probability=0.5
@@ -26,8 +27,8 @@ data_collator = DataCollatorForLanguageModeling(
 args = TrainingArguments(
     output_dir="./trainer_results",
     overwrite_output_dir=False,
-    num_train_epochs=10,  # TODO: how many? although it is limitation anyway...
-    per_device_train_batch_size=32,  # TODO: what does this?
+    num_train_epochs=10,
+    per_device_train_batch_size=32,
     save_steps=100,
     save_total_limit=3,
     seed=1337,
@@ -37,7 +38,7 @@ trainer = Trainer(
     args=args,
     data_collator=data_collator,
     train_dataset=dataset,
-    tokenizer=tokenizer
+    tokenizer=tokenizer,
 )
 trainer.train()
 trainer.save_model("./trainer_results")
