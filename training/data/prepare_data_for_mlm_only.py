65e13e6a451ca0cb3d1ef22e5d509fd5cd1f4b29
Philipp Harnisch <p.harnisch.privat@gmail.com>
2022-04-23 13:58:08 +0200

    new train variant

---
+++ training/data/prepare_data_for_mlm_only.py
@@ -0,0 +1,56 @@
+import spacy
+from datasets import load_dataset
+from tqdm.auto import tqdm
+import re
+from pathlib import Path
+import training
+
+
+def load_and_clean_for_nsp():
+    sentence_segmentation = spacy.load("en_core_web_trf")
+
+    dataset_name = "wikipedia"
+    dataset_version = "20200501.en"
+    dataset = load_dataset(dataset_name, dataset_version)
+
+    mod_path = Path(training.__file__).parent.parent
+    path = f"{mod_path}/training/data/{dataset_name}/{dataset_version}/mlm_only/"
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    documents = []
+    file_count = 0
+    for sample in tqdm(dataset['train']):
+        sample = sample['text'].replace('\n', ' ')  # "\n" -> " "
+
+        words = sample.split(" ")
+        new_words = []
+        for idx, word in enumerate(words):
+            if ":" in word:
+                continue
+            new_words.append(word)
+        sample = " ".join(new_words)
+
+        sample = sample.replace("()", " ")  # "()" -> " "
+        sample = re.sub("( +)", " ", sample)  # "( )", "(  )", ... -> " "
+        non_break_space = u'\xa0'
+        sample = sample.replace(non_break_space, " ")  # "[NBSP]" -> " "
+        sample = re.sub(" +", " ", sample)  # " ", "  ", ... -> " "
+
+        sents = [span.sent.text for span in list(sentence_segmentation(sample).sents)]
+        documents.append("\n".join(sents))
+
+        if len(documents) == 100:
+            # once we git the 10K mark, save to file
+            with open(f'{path}/text_for_nsp.txt_{file_count}', 'w', encoding='utf-8') as fp:
+                print(f"writing to file {path}/text_for_nsp.txt_{file_count} ...")
+                fp.write('\n\n'.join(documents))
+            documents = []
+            file_count += 1
+            quit()
+    with open(f'{path}/text_for_nsp_{file_count}.txt', 'w', encoding='utf-8') as fp:
+        print(f"writing to file {path}/text_for_nsp.txt_{file_count} ...")
+        fp.write('\n\n'.join(documents))
+
+
+if __name__ == "__main__":
+    load_and_clean_for_nsp()
--- training/train_model.py
+++ training/train_model.py
@@ -4,37 +4,58 @@ import torch
 from tqdm.auto import tqdm
 from pathlib import Path
 import os
+from evaluation.metrics_for_question_catalogue import GoogleREMetricCalculator, ConceptNetMetricCalculator, TRExMetricCalculator
 base_path = Path(__file__).parent.parent
-
+cnmc = ConceptNetMetricCalculator()
+gremc = GoogleREMetricCalculator()
+trmc = TRExMetricCalculator(base_path)
 
 def training_procedure(model, model_name, optimizer, training_data_rate, cuda_index, epochs, batch_size, already_trained_epochs, num_hidden_layers, learning_rate):
     device = torch.device(f"cuda:{cuda_index}") if torch.cuda.is_available() else torch.device('cpu')
     model.to(device)
     model.train()
 
-    data = get_data(TransformerType[model_name], training_data_rate)
-    loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True)
+    #data = get_data(TransformerType[model_name], training_data_rate)
+    #loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True)
+
+    transformer = Transformer.get_transformer(TransformerType[model_name])
+    tokenizer = transformer.tokenizer
+    lines_amount = int(10000 * training_data_rate)
+
+    mod_path = Path(__file__).parent.parent
+    absolute_path = str(os.path.join(str(mod_path), "training", "data", "wikipedia", "20200501.en"))
+    paths = [str(x) for x in Path(absolute_path).glob('**/*.txt')]
 
     for i in range(epochs):
         epoch = i + already_trained_epochs
-        loop = tqdm(loader, leave=True)
-        for batch in loop:
-            optimizer.zero_grad()
+        #loop = tqdm(loader, leave=True)
+        #for batch in loop:
+        for path in paths:
+            batches = make_batches_from_path(path, lines_amount, tokenizer, batch_size)
+            for batch in batches:
+                print(len(batch))
+                print("---")
+                quit()
 
-            # pull all tensor batches required for training
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            labels = batch['labels'].to(device)
+                optimizer.zero_grad()
 
-            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+                # pull all tensor batches required for training
+                input_ids = batch['input_ids'].to(device)
+                attention_mask = batch['attention_mask'].to(device)
+                labels = batch['labels'].to(device)
 
-            loss = outputs[0]  # extract loss
-            loss.backward()
-            optimizer.step()
+                outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
 
-            # print relevant info to progress bar
-            loop.set_description("Epoch " + str(epoch))
-            loop.set_postfix(loss=loss.item())
+                loss = outputs[0]  # extract loss
+                loss.backward()
+                optimizer.step()
+
+                # print relevant info to progress bar
+                #loop.set_description("Epoch " + str(epoch))
+                #loop.set_postfix(loss=loss.item())
+                print("Epoch " + str(epoch))
+                print(f"loss: {loss.item()}")
+                print("-------------------")
 
         # DELETE ALL EPOCH CHECKPOINTS (IF LAST EPOCH HAS NOT BEST SCORE, LEAVE BEST SCORE)
         absolute_path = str(os.path.join(str(base_path), "models"))
@@ -78,5 +99,60 @@ def training_procedure(model, model_name, optimizer, training_data_rate, cuda_in
             # scheduler
         }, f"{base_path}/models/{model_name}-{num_hidden_layers}-{training_data_rate}-{batch_size}-{learning_rate}-{epoch}-{round(loss.item(), 6)}-checkpoint.pth")
 
+    model.to('cpu')
+    model.eval()
+
+    k = 10
+    mq = 100
+    cnmc.get_metrics({
+        "base_path": base_path,
+        "tokenizer": tokenizer,
+        "model": model,
+        "k": k,
+        "max_questions": mq,
+        "file": "test"
+    })
+
+    model.to(device)
+    model.train()
 
 
+def make_batches_from_path(path, lines_amount, tokenizer, batch_size):
+    batches = []
+    with open(path, 'r', encoding='utf-8') as fp:
+        lines = fp.read().split('\n')[:lines_amount]
+        amount = int(lines_amount / batch_size) + 1 if lines_amount % batch_size != 0 else int(lines_amount / batch_size)
+        for b_i in range(amount):
+            lines_partial = lines[b_i*batch_size:(b_i+1)*batch_size]
+            batch = tokenizer(lines_partial, add_special_tokens=True, max_length=512, padding='max_length', truncation=True)
+
+            labels = torch.tensor(batch["input_ids"])
+            mask = torch.tensor(batch["attention_mask"])
+
+            input_ids = labels.detach().clone()
+            # create random array of floats with equal dims to input_ids
+            rand = torch.rand(input_ids.shape)
+            # mask random 15% where token is not 0 [PAD], 1 [CLS], or 2 [SEP]
+            mask_arr = (rand < .15) * (input_ids != 0) * (input_ids != 1) * (input_ids != 2)
+            # loop through each row in input_ids tensor (cannot do in parallel)
+            for i in range(input_ids.shape[0]):
+                # get indices of mask positions from mask array
+                selection = torch.flatten(mask_arr[i].nonzero()).tolist()
+                # mask input_ids
+                rand_i = torch.rand([1]).item()
+                if rand_i < 0.8:
+                    replacement = 4  # MASK token of BOTH tokenizers are currently at index 4
+                elif rand_i < 0.9:
+                    replacement = torch.randint(5, tokenizer.vocab_size, [1]).item()
+                else:
+                    replacement = input_ids[i, selection]  # do nothing, remain the token that was there
+
+                input_ids[i, selection] = replacement
+
+            batch = {
+                "input_ids": input_ids,
+                "attention_mask": mask,
+                "labels": labels
+            }
+            batches.append(batch)
+    return batches
