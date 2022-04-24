44b718b2dbfe1cdfe34628628000101d93051197
Philipp Harnisch <p.harnisch.privat@gmail.com>
2022-04-24 13:27:14 +0200

    compatibility with older alive-progress versions

--- training/train_model.py
+++ training/train_model.py
@@ -47,10 +47,10 @@ def training_procedure(model, model_name, optimizer, training_data_rate, cuda_in
                     remaining_encodings = {"input_ids":[],"attention_mask":[]}
                     batch_cnt = 0
                     while True:
-                        bar.title = f"Epoch {epoch + 1}, File {idx + 1}/{len(data_paths)}, Document {lines_amount - remaining_for_path}/{lines_amount}"
+                        #bar.title = f"Epoch {epoch + 1}, File {idx + 1}/{len(data_paths)}, Document {lines_amount - remaining_for_path}/{lines_amount}"
                         batch_cnt += 1
                         if remaining_for_path == 0 and len(remaining_encodings["input_ids"]) == 0:
-                            bar.title = f"Epoch {epoch + 1}, File {idx + 1}/{len(data_paths)}, Document {lines_amount - remaining_for_path}/{lines_amount}"
+                            #bar.title = f"Epoch {epoch + 1}, File {idx + 1}/{len(data_paths)}, Document {lines_amount - remaining_for_path}/{lines_amount}"
                             break
                         #print(f"    batch {batch_cnt} of {int(lines_amount / batch_size) + 1} ...")
                         amount = batch_size - len(remaining_encodings["input_ids"]) if remaining_for_path >= batch_size - len(remaining_encodings["input_ids"]) else remaining_for_path
@@ -136,7 +136,7 @@ def training_procedure(model, model_name, optimizer, training_data_rate, cuda_in
         model.eval()
 
         k = 10
-        mq = 100
+        mq = 1000
         cnmc.show_metrics({
             "base_path": base_path,
             "tokenizer": tokenizer,
