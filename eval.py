f74c9e405c4082fb27ad88dbb2c87254df18cef0
Philipp Harnisch <p.harnisch.privat@gmail.com>
2022-05-20 15:31:45 +0200

    flip relative example option to absolute example option (because it is the old version)

--- eval.py
+++ eval.py
@@ -17,7 +17,7 @@ def evaluate():
     parser.add_argument('checkpoint', metavar="checkpoint", type=str, help='Checkpoint within /models.')
     parser.add_argument('relation_file', metavar="relation-file", type=str, help='Relation file within subfolder of /question_dialogue.')
     parser.add_argument('-be', "--by-example", default=False, action='store_true', help='Query by example')
-    parser.add_argument('-re', "--relative-examples", default=True, action='store_true', help='If quantiles should be used rather than hard boundaries.')
+    parser.add_argument('-ae', "--absolute-examples", default=False, action='store_true', help='If quantiles should be used rather than hard boundaries.')
     parser.add_argument('-s', "--seed", default=1337, action='store', nargs='?', type=int, help='')
     parser.add_argument('-minf', "--min-freq", default=0, action='store', nargs='?', type=int, help='')
     parser.add_argument('-maxf', "--max-freq", default=100000000, action='store', nargs='?', type=int, help='')
