3b35b84eba5b70934cfa7e35ebe7a501e2e2ae54
Philipp Harnisch <p.harnisch.privat@gmail.com>
2022-07-30 16:31:43 +0200

    relation plots

--- visualizer/scatter_plot_all.py
+++ visualizer/scatter_plot_all.py
@@ -60,7 +60,10 @@ def scatter():
         filtered_dp = [x for x in dp if x["rank"] <= border_rank and x["frequency"] <= border_frequency]
         filtered_n = len(filtered_dp)
 
-        legend = r"Pearson: $\rho="+f"{pearson}"+r"$ ($p="+f"{pearson_p}"+r"$)"+"\n"+"Spearman: $\rho="+f"{spearman}"+r"$ ($p="+f"{spearman_p}"+r"$)"
+        #legend = r"Pearson: $\rho="+f"{pearson}"+r"$ ($p="+f"{pearson_p}"+r"$)"+"\n"+"Spearman: $\rho="+f"{spearman}"+r"$ ($p="+f"{spearman_p}"+r"$)"
+        legend = '\n'.join((
+            r'Pearson $\rho=%.4f$' % (pearson,),
+            r'Spearman $\rho=%.4f$' % (spearman,)))
 
         import matplotlib.pyplot as plt
 
