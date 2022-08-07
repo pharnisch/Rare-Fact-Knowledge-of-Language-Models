b0c78f061e1ccd0284257e046204b0d9565a1dc1
Philipp Harnisch <p.harnisch.privat@gmail.com>
2022-08-08 00:50:47 +0200

    interpret

--- visualizer/interpret.py
+++ visualizer/interpret.py
@@ -65,7 +65,7 @@ arr = np.asarray(all_dims)
 mask = np.zeros_like(arr)
 with sns.axes_style("white"):
     f, ax = plt.subplots(figsize=(14, 10))
-    ax = sns.heatmap(arr, cbar=False, mask=mask, vmin=-1, vmax=1, square=True, annot=True, xticklabels=x_axis_labels, yticklabels=y_axis_labels, cmap="vlag", annot_kws={"size": 20 / np.sqrt(len(all_dims))})
+    ax = sns.heatmap(arr, cbar=False, mask=mask, vmin=-1, vmax=1, annot=tkn_texts, xticklabels=x_axis_labels, yticklabels=y_axis_labels, cmap="vlag", annot_kws={"size": 20 / np.sqrt(len(all_dims))})
     plt.xticks(
         rotation=0,
         horizontalalignment='right',
@@ -77,5 +77,6 @@ with sns.axes_style("white"):
         #fontweight='light',
         fontsize='x-large'
     )
-    ax.set_yticklabels(labels=ax.get_yticklabels(), va='center')
+    ax.set(xticklabels=[])
+    ax.set_xticklabels(labels=ax.get_xticklabels(), va='center')
     plt.savefig(f"figures/interpret.png", bbox_inches='tight')
