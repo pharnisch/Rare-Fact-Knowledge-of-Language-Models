c40ab2f3b395610a576638494139ed92080a5a20
Philipp Harnisch <p.harnisch.privat@gmail.com>
2022-08-08 14:21:29 +0200

    interpret

--- visualizer/interpret.py
+++ visualizer/interpret.py
@@ -3,7 +3,7 @@ import torch
 import sys
 sys.path.append('/vol/fob-vol7/mi19/harnisph/rfk/training')
 from model_configs import TransformerType, Transformer
-transformer = Transformer.get_transformer(TransformerType["CorBert"], 12)
+transformer = Transformer.get_transformer(TransformerType["CorDistilBert"], 12)
 tokenizer = transformer.tokenizer
 model = transformer.model
 #model_name = "CorBert-12-1-4096-0.000500-9-1.359077-0.713-checkpoint.pth"
